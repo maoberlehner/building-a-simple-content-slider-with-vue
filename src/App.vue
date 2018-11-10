c396e4e18ad800fae06ad74a8cb047e459c26893
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-11-10 11:37:20 +0100

    Add tabbed content

--- src/App.vue
+++ src/App.vue
@@ -39,6 +39,76 @@
         </div>
       </SliderFrame>
     </section>
+
+    <section class="App__example o-vertical-spacing o-vertical-spacing--l">
+      <h2>Tabs</h2>
+
+      <SliderFrame>
+        <div slot-scope="{ activeIndex, goToIndex }">
+          <ul class="App__tabs-example-tabs">
+            <li>
+              <button
+                @click="goToIndex(0)"
+                :class="{ 'is-active': activeIndex === 0 }"
+                class="App__tabs-example-tab"
+              >
+                Tab 1
+              </button>
+            </li>
+            <li>
+              <button
+                @click="goToIndex(1)"
+                :class="{ 'is-active': activeIndex === 1 }"
+                class="App__tabs-example-tab"
+              >
+                Tab 2
+              </button>
+            </li>
+            <li>
+              <button
+                @click="goToIndex(2)"
+                :class="{ 'is-active': activeIndex === 2 }"
+                class="App__tabs-example-tab"
+              >
+                Tab 3
+              </button>
+            </li>
+          </ul>
+
+          <div class="App__tabs-example-content">
+            <SliderSlides>
+              <SliderSlide class="o-vertical-spacing">
+                <h3>Lorem Ipsum dolor</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren.
+                </p>
+              </SliderSlide>
+              <SliderSlide class="o-vertical-spacing">
+                <h3>Dolor Sit</h3>
+                <p>
+                  Dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren.
+                </p>
+              </SliderSlide>
+              <SliderSlide class="o-vertical-spacing">
+                <h3>Consetetur</h3>
+                <p>
+                  Consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                  justo duo dolores et ea rebum. Stet clita kasd gubergren.
+                </p>
+              </SliderSlide>
+            </SliderSlides>
+          </div>
+        </div>
+      </SliderFrame>
+    </section>
   </div>
 </template>
 
